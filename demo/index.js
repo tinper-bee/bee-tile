c9c37a33c71238f159b2aa150644fb592807eb3f
Boyuzhou <2470200492@qq.com>
2018-01-21 20:09:19 +0800

    fix: 修改示例

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,7 @@
-
-import { Con, Row, Col } from 'bee-layout';
-import { Panel } from 'bee-panel';
+import {Col, Row} from 'bee-layout';
+import {Panel} from 'bee-panel';
 import Button from 'bee-button';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 
@@ -11,38 +10,44 @@ const CARET = <i className="uf uf-arrow-down"></i>;
 const CARETUP = <i className="uf uf-arrow-up"></i>;
 
 
-var Demo1 = require("./demolist/Demo1");var Demo2 = require("./demolist/Demo2");var DemoArray = [{"example":<Demo1 />,"title":" Tile实例","code":"/**\n*\n* @title Tile实例\n* @description 通过style自定力样式，Tile的children均可自定义。hoverColors设置磁贴hover的效果\n*\n*/\n\nimport React, { Component } from 'react';\nimport Tile from 'bee-tile';\n\nclass Demo1 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<Tile style={{width:200}}>\n\t\t\t\t<h4>磁贴标题</h4>\n\t\t\t\t<span>进度</span>\n\t\t\t\t<h1>95%</h1>\n\t\t\t</Tile>\n\t\t)\t\n\t}\n}\n\n\n","desc":" 通过style自定力样式，Tile的children均可自定义。hoverColors设置磁贴hover的效果"},{"example":<Demo2 />,"title":" Tile 实例","code":"/**\n*\n* @title Tile 实例\n* @description 具备链接功能，设置参数Component为a,且设置href为你的链接地址\n*\n*/\n\nimport React, { Component } from 'react';\nimport Tile from 'bee-tile';\n\nclass Demo2 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<Tile Component=\"a\" href=\"baidu\" style={{width:200}}>\n\t\t\t\t<h4>磁贴标题</h4>\n\t\t\t\t<span>进度</span>\n\t\t\t\t<h1>95%</h1>\n\t\t\t</Tile>\n\t\t)\t\n\t}\n}\n\n","desc":" 具备链接功能，设置参数Component为a,且设置href为你的链接地址"}]
+var Demo1 = require("./demolist/Demo1");var Demo2 = require("./demolist/Demo2");var DemoArray = [{"example":<Demo1 />,"title":" Tile实例","code":"/**\n*\n* @title Tile实例\n* @description 通过style自定力样式，Tile的children均可自定义。hoverColors设置磁贴hover的效果\n*\n*/\n\nimport React, { Component } from 'react';\nimport { Tile } from 'tinper-bee';\n\nclass Demo1 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<Tile style={{width:200}}>\n\t\t\t\t<h4>磁贴标题</h4>\n\t\t\t\t<span>进度</span>\n\t\t\t\t<h1>95%</h1>\n\t\t\t</Tile>\n\t\t)\t\n\t}\n}\n\n\n","desc":" 通过style自定力样式，Tile的children均可自定义。hoverColors设置磁贴hover的效果"},{"example":<Demo2 />,"title":" Tile 实例","code":"/**\n*\n* @title Tile 实例\n* @description 具备链接功能，设置参数Component为a,且设置href为你的链接地址\n*\n*/\n\nimport React, { Component } from 'react';\nimport { Tile } from 'tinper-bee';\n\nclass Demo2 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<Tile Component=\"a\" href=\"baidu\" style={{width:200}}>\n\t\t\t\t<h4>磁贴标题</h4>\n\t\t\t\t<span>进度</span>\n\t\t\t\t<h1>95%</h1>\n\t\t\t</Tile>\n\t\t)\t\n\t}\n}\n\n","desc":" 具备链接功能，设置参数Component为a,且设置href为你的链接地址"}]
 
 
 class Demo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             open: false
         }
         this.handleClick = this.handleClick.bind(this);
     }
+
     handleClick() {
-        this.setState({ open: !this.state.open })
+        this.setState({open: !this.state.open})
     }
 
-    render () {
-        const { title, example, code, desc  } = this.props;
+    render() {
+        const {title, example, code, desc, scss_code} = this.props;
         let caret = this.state.open ? CARETUP : CARET;
         let text = this.state.open ? "隐藏代码" : "查看代码";
 
-        const footer = (
-            <Button shape="block" onClick={ this.handleClick }>
-                { caret }
-                { text }
-            </Button>
+        const header = (
+            <div>
+                {example}
+                <Button style={{"marginTop": "10px"}} shape="block" onClick={this.handleClick}>
+                    {caret}
+                    {text}
+                </Button>
+            </div>
         );
         return (
             <Col md={12}>
-                <h3>{ title }</h3>
-                <p>{ desc }</p>
-                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0,borderColor: "transparent"}} >
-                    <pre><code className="hljs javascript">{ code }</code></pre>
+                <h3>{title}</h3>
+                <p>{desc}</p>
+                <Panel collapsible headerContent expanded={this.state.open} colors='bordered' header={header}
+                       footerStyle={{padding: 0}}>
+                    <pre><code className="hljs javascript">{code}</code></pre>
+                    {!!scss_code ? <pre><code className="hljs css">{scss_code}</code></pre> : null}
                 </Panel>
             </Col>
         )
@@ -50,20 +55,22 @@ class Demo extends Component {
 }
 
 class DemoGroup extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
-    render () {
+
+    render() {
         return (
-                <Row>
-                    {DemoArray.map((child,index) => {
+            <Row>
+                {DemoArray.map((child, index) => {
 
-                        return (
-                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
-                        )
+                    return (
+                        <Demo example={child.example} title={child.title} code={child.code} scss_code={child.scss_code}
+                              desc={child.desc} key={index}/>
+                    )
 
-                    })}
-                </Row>
+                })}
+            </Row>
         )
     }
 }
